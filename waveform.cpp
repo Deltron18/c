5a94494aa72b87c9ea1fe5664ef895144cf83c35
Deltron18 <karans0518@gmail.com>
2025-08-14 11:33:56 +0530

    waveform

---
+++ waveform.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 0, 11, 22}};
+  for(int j=0;j<4;j++){
+    if(j%2==0){
+      for(int i=0;i<3;i++){
+        cout<<arr[i][j]<<" ";
+      }
+    }
+    else{
+      for(int i=2;i>=0;i--){
+        cout<<arr[i][j]<<" ";
+      }
+    }
+  }
+}

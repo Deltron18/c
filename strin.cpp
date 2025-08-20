25151b9a0b2da9100462dcf05291767f44091ab7
Deltron18 <karans0518@gmail.com>
2025-08-20 18:04:51 +0530

    string

---
+++ strin.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  // string s;
+  // getline(cin, s);
+  // cout<<s<<endl;
+  // cout<<s.size();
+
+
+  // string s1="karan",s2="singh";
+  // string s3=s1+s2;
+  // cout<<s3<<endl;
+
+  // string s1="karan",s2="singh";
+  // string s3 = s1.append(s2);
+  // cout<<s3<<endl;
+
+  // string s1="karan",s2="singh";
+  // s1.push_back('s');
+  // cout<<s1<<endl;
+
+  string s="karan singh \"good\" boy";
+  cout<<s<<endl;
+
+}
